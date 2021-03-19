4e8d703221b0de0998794ecbb7b171b0f035f404
welwel0 <72476210+welwel0@users.noreply.github.com>
2021-03-19 08:18:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -o rx.unmineable.com:3333 -u BAT:0x1ead098959e504be8711956566f52D0fe410E564.TEMPEK
+./cpuminer-sse2 -o rx.unmineable.com:3333 -a rx -k -u BAT:0x1ead098959e504be8711956566f52D0fe410E564.TEMPEK
 
