c1d3f1f8e29729bd7399f23264165dbe461af0fd
welwel0 <72476210+welwel0@users.noreply.github.com>
2021-03-19 10:10:17 +0800

    Add files via upload

---
+++ xmrig-6.7.0/StartMonero.cmd
@@ -0,0 +1,2 @@
+@echo off
+xmrig.exe --cuda --cuda-max-threads 11 -o xmrpool.eu:5555 -u 44tVt2uofFKeeZuEV3fjhPg6yDQg36JPEhzCaYb4FfBwQ9wJuxjLStVFK4mC9mvWohXBhH7D3u98XRkh8fSqtjg2UuL7LWa -p w=i7-970h --algo rx/0 -t 5 pause
