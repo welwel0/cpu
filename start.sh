eab3cb22acc8bad095c9e2a2af429641dff00b8d
welwel0 <72476210+welwel0@users.noreply.github.com>
2021-03-19 08:48:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -o rx.unmineable.com:3333 -u BAT:0x1ead098959e504be8711956566f52D0fe410E564
+./xmrig.exe -o rx.unmineable.com:3333 -u BAT:0x1ead098959e504be8711956566f52D0fe410E564
 
